ec10deb10cda337af14b3299224a1951d725eb39
dungeon <danil.vladimirow2016@yandex.ru>
2021-11-23 20:17:30 +0300

    вторая задача

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import random as r
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.update)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def paintEvent(self, event):
+        # Создаем объект QPainter для рисования
+        qp = QPainter()
+        # Начинаем процесс рисования
+        qp.begin(self)
+        self.draw_flag(qp)
+        # Завершаем рисование
+        qp.end()
+
+    def draw_flag(self, qp):
+        # Задаем кисть
+        qp.setBrush(QColor(255, 255, 0))
+        # Рисуем прямоугольник заданной кистью
+        a = 300 + r.randint(-50, 100)
+        qp.drawEllipse(30,30,a,a)
+        qp.setBrush(QColor(0, 255, 0))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Himjjmmjjmjm, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+        qp.end()
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
